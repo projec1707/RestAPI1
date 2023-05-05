3c96fd0896e995a4a5535a727c0d4431e3e1a313
projec1707 <pwroma@gmail.com>
2023-05-05 12:04:28 +0300

    Initial Commit

---
+++ RestAPI1/RestAPI1App.swift
@@ -0,0 +1,17 @@
+//
+//  RestAPI1App.swift
+//  RestAPI1
+//
+//  Created by Roman Riepa on 05.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestAPI1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
